fe89c4ecb87814312285579fc192d6f13c1c0ebb
Shashank Verma <Shashank.Verma1@BRILLIO.COM>
2022-08-17 15:29:02 +0530

    Email Validation in register and navigation to login

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AppService } from 'src/app/app.service';
 import { Plan } from 'src/app/Plan';
 import { User } from 'src/app/User';
@@ -11,12 +12,12 @@ import { User } from 'src/app/User';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private appservice: AppService) { }
+  constructor(private appservice: AppService, private route: Router) { }
 
   ngOnInit(): void {
   }
 
-  user : User = new User;
+  user: User = new User;
 
   password: string | undefined;
   confirmPassword: string | undefined;
@@ -27,12 +28,28 @@ export class RegisterComponent implements OnInit {
       alert("Passwords mismatch");
       return;
     }
+    if (!this.ValidateEmail(this.user.email)) {
+      alert("Invalid email id");
+      return;
+    }
     this.user.password = this.password;
     this.user.gender = "M"; //need to change this
     console.log(this.user);
     this.appservice.registerUser(this.user).subscribe(data => {
       console.log("Successfully registered!");
     });
+    this.route.navigateByUrl("/login");
     // console.log(res);
+  }
+
+  //validation of email
+  ValidateEmail(uemail: any) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (uemail.match(mailformat)) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
