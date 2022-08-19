0e9c7a5395b3830ba0615b0a18be237d4f6c9a48
Aman Sahu <Aman.Sahu@BRILLIO.COM>
2022-08-19 15:54:52 +0530

    minor change

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { User } from './User';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { Diet } from './Diet';
+import { Workout } from './Workout';
+import { Plan } from './Plan';
 
 
 @Injectable({
@@ -13,7 +16,7 @@ export class AppService {
   private baseURL = "http://localhost:8080/"
   constructor(private http: HttpClient, private route: Router) { }
 
-  globalloggedinuser : any;//currently logged in user
+  globalloggedinuser: any;//currently logged in user
 
 
   //to get all the users
@@ -24,20 +27,32 @@ export class AppService {
 
 
   //to send a post request for login 
-  loginUser(email:string,password:string):Observable<any>{
-    const headers = {'content-type' : 'application/json'}
-    const body = JSON.stringify({email,password});
+  loginUser(email: string, password: string): Observable<any> {
+    const headers = { 'content-type': 'application/json' }
+    const body = JSON.stringify({ email, password });
     console.log("[Inside appservice]This is the data we are sending to spring" + body);
-    return this.http.post<User[]>(this.baseURL + 'login',body,{'headers':headers})
-    
+    return this.http.post<User[]>(this.baseURL + 'login', body, { 'headers': headers })
+
   }
 
- 
+
 
   //service for registering a user
   registerUser(user: User): Observable<any> {
     console.log("Inside the register user post request");
     return this.http.post(this.baseURL + 'users', user);
     // this.route.navigateByUrl("/profilepage");
+  }
+
+  getDiet(dietId: number): Observable<Diet> {
+    return this.http.get<Diet>(this.baseURL + 'diets/' + dietId);
+  }
+
+  getWorkout(workoutId: number): Observable<Workout> {
+    return this.http.get<Workout>(this.baseURL + 'workouts/' + workoutId);
+  }
+
+  getPlan(plan_id: number): Observable<Plan> {
+    return this.http.get<Plan>(this.baseURL + 'plans/' + plan_id);
   }
 }
--- src/app/plans/plans/plans.component.ts
+++ src/app/plans/plans/plans.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Plan } from 'src/app/Plan';
+import { Diet } from 'src/app/Diet';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-plans',
@@ -7,13 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlansComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appservice: AppService) { }
 
   ngOnInit(): void {
   }
 
   //just to check whether click is working or not.
-  afterclick(){
-    alert("Click is working");
+  // afterclick(){
+  //   alert("Click is working");
+  // }
+
+  plan: Plan = new Plan;
+  addPlan(plan_id: number) {
+    this.appservice.getPlan(plan_id).subscribe(data => {
+      this.plan = data;
+      console.log("Check Plan", this.plan);
+    })
+
+    // console.log("Calling Diet :");
+    // // this.plan.diet = this.appservice.getDiet(plan_id);
+    // this.appservice.getDiet(plan_id).subscribe((data) => {
+    //   this.plan.diet = data;
+    //   console.log("Check Diet : ", this.plan);
+    // })
+
+    // console.log("Calling Workout :");
+    // this.appservice.getWorkout(plan_id).subscribe((data) => {
+    //   this.plan.workout = data;
+    //   console.log("Check Workout : ", this.plan);
+    // })
   }
+
 }
