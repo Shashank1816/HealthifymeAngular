8c166fcd95a69a838d30b4a48589148f05a52b33
Shashank Verma <Shashank.Verma1@BRILLIO.COM>
2022-08-21 16:17:08 +0530

    setup dummy chart using ng2charts

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalyticspageComponent } from './analyticspage/analyticspage.component';
 import { NavbarModule } from '../navbar/navbar.module';
+import { GoogleChartsModule } from 'angular-google-charts';
+import { FormsModule } from '@angular/forms';
+import { NgChartsModule } from 'ng2-charts';
 
 
 
@@ -11,7 +14,13 @@ import { NavbarModule } from '../navbar/navbar.module';
   ],
   imports: [
     CommonModule,
-    NavbarModule
+    NavbarModule,
+    GoogleChartsModule,
+    FormsModule,
+    NgChartsModule
+  ],
+  exports:[
+    AnalyticspageComponent
   ]
 })
 export class AnalyticsModule { }
--- src/app/analytics/analyticspage/analyticspage.component.ts
+++ src/app/analytics/analyticspage/analyticspage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
+
 
 @Component({
   selector: 'app-analyticspage',
@@ -9,6 +11,23 @@ export class AnalyticspageComponent implements OnInit {
 
   constructor() { }
 
+  title = 'ng2-charts-demo';
+
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+    ]
+  };
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: false,
+  };
+  
   ngOnInit(): void {
     
   }
@@ -20,7 +39,7 @@ export class AnalyticspageComponent implements OnInit {
 
   }
   getCalorieArrays(){
-    
+
   }
   
 
